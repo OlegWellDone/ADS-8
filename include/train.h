ecbdf4bb57685f460a55f9a9db6e7b7ac93b8414
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2022-05-08 22:00:01 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -9,13 +9,13 @@ class Train {
     Cage *next;
     Cage *prev;
   };
-  int countOp, length, count; // счетчик шагов 
-  Cage *first, *current; // первый вагон и текущий при входе
+  int countOp, length, count; //счетчик шагов
+  Cage *first, *current; //первый вагон и текущий при входе
   Train :: Cage *create(bool light);
  public:
   Train();
-  void addCage(bool light); 
-  int getLength();          
-  int getOpCount();         
+  void addCage(bool light);
+  int getLength();
+  int getOpCount();
 };
 #endif  // INCLUDE_TRAIN_H_
